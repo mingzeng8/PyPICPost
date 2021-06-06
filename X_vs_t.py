ad5df530614229e1ced204bb75f7f61833bd4924
Ming Zeng <zengming@ihep.ac.cn>
2021-06-06 16:45:14 +0800

    Add support for sliced output of OSIRIS.

--- X_vs_t.py
+++ X_vs_t.py
@@ -15,13 +15,13 @@ def get_field_peak(outfile_object, if_abs=True):
     else:
         return np.max(outfile_object._data)
 
-def plot_field_peak_vs_t(path, code_name='osiris', field_name='e3', if_abs=False, use_num_list=False, start=0, count=0, stride=1, t_offset = 0., h_ax=None, field_abs=True, **kwargs):
+def plot_field_peak_vs_t(path, code_name='osiris', field_name='e3', fld_slice=None, if_abs=False, use_num_list=False, start=0, count=0, stride=1, t_offset = 0., h_ax=None, field_abs=True, **kwargs):
     '''
     
     '''
     t_array = list()
     peak_array = list()
-    outfile_object = outfile.OutFile(code_name=code_name, path=path, field_name=field_name, use_num_list=use_num_list)
+    outfile_object = outfile.OutFile(code_name=code_name, path=path, field_name=field_name, fld_slice=fld_slice, use_num_list=use_num_list)
     for i in range(start, start+count*stride, stride):
         try:
             outfile_object.out_num=i
@@ -41,11 +41,11 @@ def plot_field_peak_vs_t(path, code_name='osiris', field_name='e3', if_abs=False
     return h_ax
 
 if __name__ == '__main__':
-    h_ax = plot_field_peak_vs_t(code_name='fbpic', path='/home/zengming/project/FB/PTE_k28/diag_k=28_a0=15.7_w0=2.4_d=0', field_name='e2', if_abs=True, use_num_list=True, count=999, color='k', label='$d=0$')
+    '''h_ax = plot_field_peak_vs_t(code_name='fbpic', path='/home/zengming/project/FB/PTE_k28/diag_k=28_a0=15.7_w0=2.4_d=0', field_name='e2', if_abs=True, use_num_list=True, count=999, color='k', label='$d=0$')
     plot_field_peak_vs_t(code_name='fbpic', path='/home/zengming/project/FB/PTE_k28/diag_k=28_a0=15.7_w0=2.4_d=56', field_name='e2', if_abs=True, use_num_list=True, count=999, h_ax = h_ax, color='r', label='$d=56$')
     plot_field_peak_vs_t(code_name='fbpic', path='/home/zengming/project/FB/PTE_k28/diag_k=28_a0=15.7_w0=2.4_d=60', field_name='e2', if_abs=True, use_num_list=True, count=999, h_ax = h_ax, color='b', label='$d=60$')
     plot_field_peak_vs_t(code_name='fbpic', path='/home/zengming/project/FB/PTE_k28/diag_k=28_a0=15.7_w0=2.4_d=70', field_name='e2', if_abs=True, use_num_list=True, count=999, h_ax = h_ax, color='g', label='$d=70$')
-    plot_field_peak_vs_t(code_name='fbpic', path='/home/zengming/project/FB/PTE_k28/diag_k=28_a0=15.7_w0=2.4_d=100', field_name='e2', if_abs=True, use_num_list=True, count=999, h_ax = h_ax, color='y', label='$d=100$')
+    plot_field_peak_vs_t(code_name='fbpic', path='/home/zengming/project/FB/PTE_k28/diag_k=28_a0=15.7_w0=2.4_d=100', field_name='e2', if_abs=True, use_num_list=True, count=999, h_ax = h_ax, color='y', label='$d=100$')'''
     '''h_ax = plot_field_peak_vs_t(code_name='fbpic', path='/home/zengming/project/FB/PTE/diag_k=25_a0=20.3_w0=2.7_d=0', field_name='e2', if_abs=True, use_num_list=True, count=999, color='k', label='$d=0$')
     plot_field_peak_vs_t(code_name='fbpic', path='/home/zengming/project/FB/PTE/diag_k=25_a0=20.3_w0=2.7_d=80', field_name='e2', if_abs=True, use_num_list=True, count=999, h_ax = h_ax, color='r', label='$d=80$')
     h_ax = plot_field_peak_vs_t(code_name='fbpic', path='/home/zengming/project/FB/PTE/diag_k=25_a0=20.3_w0=2.7_d=50', field_name='e2', if_abs=True, use_num_list=True, count=999, color='y', label='$d=50$')
@@ -53,8 +53,9 @@ if __name__ == '__main__':
     plot_field_peak_vs_t(code_name='fbpic', path='/home/zengming/project/FB/PTE/diag_k=25_a0=20.3_w0=2.7_d=70', field_name='e2', if_abs=True, use_num_list=True, count=99, h_ax = h_ax, color='g', label='$d=70$')
     plot_field_peak_vs_t(code_name='fbpic', path='/home/zengming/project/FB/PTE/diag_k=25_a0=20.3_w0=2.7_d=80', field_name='e2', if_abs=True, use_num_list=True, count=999, h_ax = h_ax, color='r', label='$d=80$')
     plot_field_peak_vs_t(code_name='fbpic', path='/home/zengming/project/FB/PTE/diag_k=25_a0=20.3_w0=2.7_d=90', field_name='e2', if_abs=True, use_num_list=True, count=999, h_ax = h_ax, color='b', label='$d=90$')'''
-    plt.xlabel('$t$ [s]')
-    plt.ylabel('$E_L$ [V/m]')
+    plot_field_peak_vs_t(code_name='osiris', path='/public3/home/sc55489/jobs/2', field_name='e3', fld_slice=3, if_abs=True, use_num_list=True, count=999, color='r', label='')
+    plt.xlabel('$t$')
+    plt.ylabel('$E_L$')
     plt.legend()
     plt.tight_layout()
     plt.show()
